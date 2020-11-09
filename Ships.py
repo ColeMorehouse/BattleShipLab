a29ac351cf9d3db9744951d947329ab3cf2225df
cbrinkman1 <72221396+cbrinkman1@users.noreply.github.com>
2020-11-09 12:34:59 -0500

    Create Ships.py

---
+++ Ships.py
@@ -0,0 +1,22 @@
+class ships:
+
+    def __init__(self, t, d):
+        type = t
+        direction = d
+        lengths = {
+            "D": 2,
+            "S": 3,
+            "C": 3,
+            "B": 4,
+            "A": 5
+        }
+        length = lengths[t]
+
+    def getLength(self):
+        return self.length
+
+    def getType(self):
+        return self.type
+
+    def getDirection(self):
+        return self.direction
