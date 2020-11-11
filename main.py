65405649424ff6d18d2b14bec99cfad4ef931520
Edelaville <72936743+Edelaville@users.noreply.github.com>
2020-11-11 13:01:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,5 +21,19 @@ player2.setUpBoard()
 notwin = true
 
 while notwin:
-  yield
+  for x in range(10):
+    row = input("Player 1! Enter row of guess: ")
+    col = input("Player 1! Enter column of guess: ") 
+    gRet = p2b.guess(row, col)
+    if gRet == "out of bounds" or gRet == "already guessed":
+      x -= 1
+      print (gRet)
+    else:
+      p1gb.doGuess(p2b)
+      
+      p1b.printBoard()
+      p1gb.printGB()
+      p2b.printBoard()
+      p2gb.printGB()
+    
  # add shit here
