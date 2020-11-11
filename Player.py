91876c86dcf89fe65a4bbc7f5a03f874fee0b8a5
Edelaville <72936743+Edelaville@users.noreply.github.com>
2020-11-11 12:14:21 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,23 +1,41 @@
-from Board import Board
-from Ships import Ships
+rom board import board
+from ships import ships
 
 class Player:
     def __init__(self, b1, b2):
         self.board1 = b1
         self.board2 = b2
 
-    def placeShips(self, ship, row, col):
-        dict = {
-            "down": row <= 10 - ship.getLength() and row >= 0,
-            "right": col <= 10 - ship.getLength() and col >= 0
-        }
-        if ship.getType() == "D":
-            
-        if ship.getType() == "S" or ship.type == "C":
+    def setUpBoard(self):
+        ddirect = input("Enter direction of Destroyer: ")
+        dest = Ship("D", ddirect)
+        dposx = input("Enter x coordinate of Destroyer: ")
+        dposy = input("Enter y coordinate of Destroyer: ")
+        placeShip(dest,dposx, dposy)
 
-        if ship.getType() == "B":
+        sdirect = input("Enter direction of Submarine: ")
+        sub = Ship("S",sdirect)
+        sposx = input("Enter x coordinate of Submarine: ")
+        sposy = input("Enter y coordinate of Submarine: ")
+        placeShip(sub,sposx, sposy)
 
-        if ship.getType() == "A":
+        cdirect = input("Enter direction of Cruiser: ")
+        cruise = Ship("C",cdirect)
+        cposx = input("Enter x coordinate of Cruiser: ")
+        cposy = input("Enter y coordinate of Cruiser: ")
+        placeShip(cruise,cposx, cposy)
+
+        bdirect = input("Enter direction of BattleShip: ")
+        battle = Ship("B", bdirect)
+        bposx = input("Enter x coordinate of BattleShip: ")
+        bposy = input("Enter y coordinate of BattleShip: ")
+        placeShip(battle, bposx, bposy)
+
+        adirect = input("Enter direction of Aircraft Carrier: ")
+        aircraft = Ship("C", cdirect)
+        aposx = input("Enter x coordinate of Aircraft Carrier: ")
+        aposy = input("Enter y coordinate of Aircraft Carrier: ")
+        placeShip(aircraft, aposx, aposy)
 
 
     def recieveGuess(self, row, column):
