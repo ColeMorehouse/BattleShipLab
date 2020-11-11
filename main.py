8e5bbdd89850b5d483e86df34ba3d3f3c3cdc969
Edelaville <72936743+Edelaville@users.noreply.github.com>
2020-11-11 12:32:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from Player import Player
 
 #Create a board for player 1 and player
 p1b = Board() #test changegit
-p1gb = GBoard()
+p1gb = gBoard()
 
 p2b = Board()
-p2gb = GBoard()
+p2gb = gBoard()
 
-player1 = Player(b1h, b1v)
+player1 = Player(p1b, p1gb)
 #Create a hidden board for player 1 and player 2
 #Create player 1 and player 2
+player2 = p2(p2b, p2gb)
 
-player2 = p2(b2h, b1v)
