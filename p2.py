42a938a3a3c0ee470a09c18e7b6dc5b3a3b8d7a6
ColeMorehouse <72094244+ColeMorehouse@users.noreply.github.com>
2020-11-11 12:28:27 -0500

    Updated p2

--- p2.py
+++ p2.py
@@ -1,11 +1,7 @@
 class p2:
 
-    def __init__(self, Board, D, S, C, B, A):
-        Destroyer = D
-        Submarine = S
-        Cruiser = C
-        Battleship = B
-        AircraftCarrier = A
-        Board = Board
+    def __init__(self, Board1, Board2):
+        pass
+
 
 
